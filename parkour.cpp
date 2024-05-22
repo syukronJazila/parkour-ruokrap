6dc91f9bb88ced8b8dabd052b801220a5f19f8da
Muhammad Syukron Jazila <139365360+syukronJazila@users.noreply.github.com>
2024-05-22 20:47:26 +0700

    perbaiki bug jurang parkour.cpp

--- parkour.cpp
+++ parkour.cpp
@@ -5,26 +5,28 @@ using namespace std;
 main(){
     int i = 0, n = 30,r;
     string arr[n],arr2[n];
+
     for (int j = 0; j < n; j++)
     {
         arr[j] = '#';
         arr2[i] = " ";
     }
+
     bool isObstacle = false;
     int obstacle = 0;
 	while (true)
     {
     system("CLS");
     r = rand() % 100 + 1;
-    if(obstacle >= 3 && obstacle <= 4){
+    if(obstacle >= 2 && obstacle <= 3){
         r = 50;
         obstacle++;
-        if (obstacle == 5){ obstacle = 0;}
+        if (obstacle == 4){ obstacle = 0;}
     }
-    if(isObstacle && obstacle == 2){
+    if(isObstacle && obstacle == 1){
         r = 50;
         isObstacle = false;
-        obstacle = 3;
+        obstacle = 2;
     }
     if(r > 66 ){
         for (int j = n-1; j >= 0; j--)
@@ -47,7 +49,6 @@ main(){
             arr[j] = arr[j-1];
             arr2[j] = arr2[j-1];        
         }
-        
     }
     else{
         isObstacle = true;
