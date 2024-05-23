32b9cb2051424c4c45db65f09aaafbb77640e739
Muhammad Syukron Jazila <139365360+syukronJazila@users.noreply.github.com>
2024-05-23 12:05:06 +0700

    tambah fitur dinding banyak parkour.cpp
    banyak dinding di rentang 4 - 7 (tergantung angka random)

--- parkour.cpp
+++ parkour.cpp
@@ -2,6 +2,13 @@
 #include <windows.h>
 using namespace std;
 
+void gotoxy(int x, int y) {
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+
 main(){
     int i = 0, n = 30,r;
     string arr[n],arr2[n];
@@ -11,10 +18,11 @@ main(){
         arr[j] = '#';
         arr2[j] = " ";
     }
-
+    
     bool isObstacle = false;
     bool isWall = false;
     int obstacle = 0;
+    int nWall = 0;
 	while (true)
     {
         system("CLS");
@@ -30,35 +38,44 @@ main(){
             obstacle = 2;
         }
         if(isWall){
-            r = 50;
-            isWall = false;
-            obstacle = 2;
+            if (r % 2 == 0){
+                nWall++;
+            }
+            // else{ kalau ingin dindingnya lebih panjang
+            //     nWall--;
+            // } kalau tanpa else 4 - 7 dinding
+            nWall++;
+            r = 87;
+            if(nWall > 6){
+                isWall = false;
+                obstacle = 2;
+                nWall = -1;
+                r = 50;
+            }
         }
 
         for (int j = n-1; j >= 0; j--)
-            {
-                if(j == 0){
-                    arr[0] = "#";
-                    arr2[0] = " ";
-                    break;
-                }
-                arr[j] = arr[j-1];
-                arr2[j] = arr2[j-1];        
+        {
+            if(j == 0){
+                arr[0] = "#";
+                arr2[0] = " ";
+                break;
             }
+            arr[j] = arr[j-1];
+            arr2[j] = arr2[j-1];        
+        }
 
-        if(r > 66 ){
+        if(r > 80 ){
             isWall = true;
             arr2[0] = "#";
-        }
-        else if(r > 33){
+        }else if(r > 33){
             arr[0] = "#";
-            
         }else{
             isObstacle = true;
             obstacle++;
             arr[0] = " ";
         }
-
+        gotoxy(0,10);
         for(const string &a: arr2){
             cout << a;
         }
