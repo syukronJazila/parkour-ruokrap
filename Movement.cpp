a2d34f7b7adf69f65d1607a7c3e47bcdca448c39
Muhammad Dzakwan Attaqiy <98676984+mhddzakwan@users.noreply.github.com>
2024-06-08 10:32:53 +0700

    nambahi gerakan player

--- Movement.cpp
+++ Movement.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-#include <vector>
 #include <windows.h>
+#include <vector>
+#include <conio.h>
 #include <thread>
- 
 using namespace std;
+
+const int n = 50;
+string ground1[n],ground2[n],ground3[n],ground4[n],ground5[n]; 
 
 struct Player{
 	int posX,posY,LposX,LposY;
@@ -22,12 +25,59 @@ Ammo tempPeluru;
 //? deklarasi player
 Player p1;
 bool sudah=true;
+
 void gotoxy(int x, int y) {
-    COORD coord;  
+    COORD coord;
     coord.X = x;
     coord.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+
+
+
+void setGround(string a,string b,string c,string d,string e){
+    ground1[0] = a; 
+    ground2[0] = b; 
+    ground3[0] = c; 
+    ground4[0] = d; 
+    ground5[0] = e; 
+}
+
+void printGround(){
+    gotoxy(0,7);
+    for(int j = 0; j < n; j++){
+        cout << ground5[j];
+    }
+    gotoxy(0,8);
+    for(int j = 0; j < n; j++){
+        cout << ground4[j];
+    }
+    gotoxy(0,9);
+    for(int j = 0; j < n; j++){ 
+        cout << ground3[j];
+    }
+    gotoxy(0,10);
+    for(int j = 0; j < n; j++){ 
+        cout << ground2[j];
+    }
+    gotoxy(0,11);
+    for(int j = 0; j < n; j++){ 
+        cout << ground1[j];
+    }
+}
+
+void shiftGround(){
+    // menggeser tiap elemen ground ke kanan agar tampak seperti berjalan (sepert queue)
+    for (int j = n-1; j >= 0; j--)
+    {
+        ground1[j] = ground1[j-1];
+        ground2[j] = ground2[j-1];        
+        ground3[j] = ground3[j-1];        
+        ground4[j] = ground4[j-1];        
+        ground5[j] = ground5[j-1];        
+    }
+}
+
 void pindahPosisi(){
 
 	gotoxy(p1.LposX, p1.LposY); cout << " ";  // Hapus karakter lama
@@ -35,13 +85,11 @@ void pindahPosisi(){
     p1.LposX = p1.posX;  // Simpan posisi lama
     p1.LposY = p1.posY;
 }
-
-
+int kiri = 0;
 void cekTombol() {
     while (true) {
-        bool moved = false;
 		int i;
-        if (sudah == true){
+
             //? Buat cek tombok ke atas
             if (GetAsyncKeyState(VK_UP) & 0x8000) {
                 if (p1.posY > 0) {
@@ -49,85 +97,138 @@ void cekTombol() {
                         //? Minus artinya naik
                         p1.posY -= 1;
                         pindahPosisi();
-                        Sleep(50);
-                    }
+                        Sleep(115);
+                }
+                
                     for (i=1; i<=3; i++){
                         p1.posY += 1;
                         pindahPosisi();
-                        Sleep(50);
+                        Sleep(100);  
                     }
                 }
+                 
             }
-            //? Buat cek tombok ke kiri
-            if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
-                if (p1.posX > 0) {
-                    //? gerak ke kiri
-                    p1.posX -= 1;
-                    pindahPosisi(); 
-                }
+            // Cek tombol ke kiri
+        if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
+            if (p1.posX > 0) {
+                p1.posX -= 1;
+                pindahPosisi();
+                Sleep(100);
             }
-            if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
-                if (p1.posX < 79) {  // Assuming console width is 80
-                    p1.posX += 1;
-                    pindahPosisi();
-                }
-            }
-            
-            //? peluru muncul klo spasi di tekan
-            if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
-            //? tampilkan peluru di depan karakter
-            gotoxy(p1.posX-1,p1.posY); cout << "-";
-            tempPeluru.posX = p1.posX-1;
-            tempPeluru.posY = p1.posY;
-            tempPeluru.LposX = p1.posX-1;
-            tempPeluru.LposY = p1.posY;
-            //? masukkan peluru ke vektor
-            peluru.push_back(tempPeluru);
-            
-            }
-
-            Sleep(100);  // Menghindari penggunaan CPU yang tinggi
         }
+        // Cek tombol ke kanan
+        if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
+            if (p1.posX < 79) {  // Asumsi lebar konsol adalah 80
+                p1.posX += 1;
+                pindahPosisi();
+                Sleep(100);
+            }
+        }
+            Sleep(100); 
     }
 }
 
 
-
-//? Disini ntar klo mau build tempatnya, sekalian gerakan peluru
-void programLain(){
-	while(true){
-            int j;
-            //? Kita gerakkan setiap peluru
-            for (j=peluru.size()-1; j>=0; j--){
-                sudah = false;
-                gotoxy(peluru[j].LposX, peluru[j].LposY); cout << " ";  // Hapus karakter lama
-                peluru[j].posX--;
-                if (peluru[j].posX == 3) {
-                    //gotoxy(a.posX, a.posY); cout << " ";
-                    peluru.erase(peluru.begin() + j);
-                    sudah=true;
-                }else {
-                    gotoxy(peluru[j].posX, peluru[j].posY); cout << "-";    // Cetak karakter di posisi baru
-                    peluru[j].LposX = peluru[j].posX;  // Simpan posisi lama
-                    peluru[j].LposY = peluru[j].posY;
-                    sudah = true;
-                }
-                
-            }
-            Sleep(400);
-	} 
-}
-
 main(){
-    system("cls");
-    //? pakek thread, jadi ada 2 program yg bekerja di waktu bersamaan, cekTombol() dan main() program
-	thread threadPemeriksaanTombol(cekTombol);
-	gotoxy(1,10); cout <<"##########################################";
-	
-	p1.posX = 5;
-	p1.posY = 9;
-	p1.LposX = 5;
-	p1.LposY = 9;
-	gotoxy(p1.posX,p1.posY); cout << "A";
-	programLain();
+    thread threadPemeriksaanTombol(cekTombol);
+    int i = 0,r,k=0,groundLvl=2;
+    char c;
+    p1.posX = 35;
+	p1.posY = 10;
+	p1.LposX = 35;
+	p1.LposY = 10;
+    gotoxy(p1.posX,p1.posY); cout << "A";
+    // inisialisasi ground dengan nilai default
+    for (int j = 0; j < n; j++)
+    {
+        ground1[j] = '#'; 
+        ground2[j] = " "; 
+        ground3[j] = " "; 
+        ground4[j] = " "; 
+        ground5[j] = " "; 
+    }
+    
+    bool isJurang = false;
+    bool isWall = false;
+    int jurang = 0;
+    int nWall = 0;
+	while (true)
+    {
+        system("CLS");gotoxy(0,3); cout << "kiri = " << kiri;  
+        k++;
+        r = rand() % 100 + 1; // cari angka random 1 - 100
+
+        shiftGround();
+
+        if(jurang >= 2 && jurang <= 5){ 
+         // untuk memastikan bahwa setelah muncul jurang jurang 4 tanah setelahnya 
+         // tidak muncul jurang lain
+            isJurang = false;
+            if(r <= 33){
+                r = 61;
+            }
+            jurang++;
+            if (jurang == 6){ jurang = 0;}
+        }
+        if(isJurang){ // mengecek apakah muncul jurang 
+            r = 14; // jika iya maka buat jurang kedua
+        }
+        if(isWall){ 
+            if(r < 15 && nWall > 4 && nWall < 11){
+                goto skip;
+            }
+            if (r % 2 == 0){ // jika angka random genap maka banyak dinding dikurang satu
+                nWall++; 
+            }
+            nWall++; 
+            if(nWall > 13){ // batas kemunculan dinding adalah 13 (karena nWall bisa bernilai 1 - 13)
+                nWall = 0;
+                if((r < 50 && groundLvl > 2) || groundLvl == 5){ 
+                    groundLvl--; // mengurangi level ground
+                }else if(r > 60){
+                    groundLvl++; // menambah level ground
+                }else{ // buat jadi else if groundLvl == 2 jika hanya ingin lvl2 ke lvl 1
+                    r = 34; // obstacle baru 
+                    goto skip;
+                }
+            }
+            r=51;
+        }
+
+        skip:
+        if(r > 50 ){ // kalau angka random 50 - 100 maka muncul dinding
+            switch (groundLvl)
+                {
+                case 2:
+                    setGround("#","#"," "," "," ");
+                    break;
+                case 3:
+                    setGround("#","#","#"," "," ");
+                    break;
+                case 4:
+                    setGround("#","#","#","#"," ");
+                    break;
+                case 5:
+                    setGround("#","#","#","#","#");
+                    break;
+
+                default: 
+                    break;
+                }
+            isWall = true;
+        }else if(r > 33){ // 34 - 50 set default
+            setGround("#"," "," "," "," ");
+        }else{ // 0 - 33 muncul jurang 
+            isJurang = true;
+            jurang++;
+            setGround(" "," "," "," "," ");
+        }
+        gotoxy(p1.posX,p1.posY); cout << "A" ;
+        printGround();
+        gotoxy(0,0);cout << k;
+        Sleep(200);
+    }
+    threadPemeriksaanTombol.join();
+    c = getch();
+    
 }
